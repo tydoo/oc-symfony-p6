b7248544a93ab88a12930bf18cfe854064abc63a
BOYER Thomas <thomas.boyer2@interieur.gouv.fr>
2024-01-27 11:12:56 +0100

    ✅ Ajout du controller home

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController {
+
+    #[Route('/', name: 'home.index', methods: ['GET'])]
+    public function index(): RedirectResponse {
+        return $this->redirectToRoute('home.home');
+    }
+
+    #[Route('/home', name: 'home.home', methods: ['GET'])]
+    public function home(): Response {
+        return $this->render('home/index.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }
+}
