2d00ad802c954ef3f702db75538ef6de4dbb9053
tboyer <thomas.boyer2@interieur.gouv.fr>
2024-04-13 20:02:03 +0200

    ✅ Ajout contrainte d'email

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -4,14 +4,13 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Validator\Constraints\IsTrue;
+use Symfony\Component\Validator\Constraints\Email;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
 
@@ -42,6 +41,9 @@ class RegistrationFormType extends AbstractType {
                     new Length([
                         'max' => 180,
                         'maxMessage' => 'Votre adresse email ne peut pas contenir plus de {{ limit }} caractères',
+                    ]),
+                    new Email([
+                        'message' => 'Veuillez saisir une adresse email valide.',
                     ]),
                 ],
                 'help' => 'Votre adresse email ne peut pas contenir plus de 180 caractères.'
