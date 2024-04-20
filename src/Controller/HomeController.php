bd9ce01a34f7b7a3d53807a126d77400f36defc2
tboyer <thomas.boyer2@interieur.gouv.fr>
2024-04-20 17:53:02 +0200

    ✅ Ajout btn pour charger plus de tricks > 15 #14

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,10 +3,11 @@
 namespace App\Controller;
 
 use App\Repository\FigureRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpKernel\Attribute\MapQueryParameter;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomeController extends AbstractController {
 
@@ -16,9 +17,20 @@ class HomeController extends AbstractController {
     }
 
     #[Route('/home', name: 'home.home', methods: ['GET'])]
-    public function home(FigureRepository $figureRepository): Response {
+    public function home(
+        FigureRepository $figureRepository,
+        #[MapQueryParameter] ?string $page,
+    ): Response {
+        $page = $page ?: 1;
+
+        $figureCount = $figureRepository->count([]);
+        $figures = $figureRepository->findBy([], ['createdAt' => 'DESC'], 15 * $page);
+        $lastPage = (int) ceil($figureCount / 15);
+
         return $this->render('home.html.twig', [
-            'figures' => $figureRepository->findAll(),
+            'figures' => $figures,
+            'page' => $page,
+            'lastPage' => $lastPage,
         ]);
     }
 }
